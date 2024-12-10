4834c9cb9d8676079b52d2d11e93e2f5d9357902
Sujala Rai <sujala.rai2004@gmail.com>
2024-12-10 10:47:11 +0545

    calculator

---
+++ calculator/lib/view/calculator.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+
+class Calculator extends StatefulWidget {
+  const Calculator({super.key});
+
+  @override
+  State<Calculator> createState() => _CalculatorState();
+  // final _textController = TextEditingController();
+}
+
+class _CalculatorState extends State<Calculator> {
+  List<String> lstSymbols = [
+    "C",
+    "*",
+    "/",
+    "<-",
+    "1",
+    "2",
+    "3",
+    "+",
+    "4",
+    "5",
+    "6",
+    "-",
+    "7",
+    "9",
+    "*",
+    "%",
+    "0",
+    ".",
+    "="
+  ];
+  final _key = GlobalKey<FormState>();
+  int first = 0;
+  int second = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Calculator App'),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(8.0),
+        child: Form(
+          key: _key,
+          child: Column(
+            children: [
+              TextFormField(
+                textDirection: TextDirection.rtl,
+                // controller: _textController,
+                decoration: const InputDecoration(
+                  border: OutlineInputBorder(),
+                ),
+                style: const TextStyle(
+                  fontSize: 20,
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(
+                height: 8,
+              ),
+              Expanded(
+                child: GridView.builder(
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 4,
+                    crossAxisSpacing: 8.0,
+                    mainAxisSpacing: 8.0,
+                  ),
+                  itemCount: lstSymbols.length,
+                  itemBuilder: (context, index) {
+                    return ElevatedButton(
+                      style: ElevatedButton.styleFrom(
+                        backgroundColor: Colors.amber,
+                        shape: RoundedRectangleBorder(
+                          borderRadius: BorderRadius.circular(0),
+                        ),
+                      ),
+                      onPressed: () {},
+                      child: Text(
+                        lstSymbols[index],
+                        style: const TextStyle(
+                          fontSize: 24,
+                          color: Colors.black,
+                          fontWeight: FontWeight.bold,
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+// use only one controller
+//  substring for deleting
+//3 variable first secound and operator
